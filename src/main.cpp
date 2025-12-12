80026b32b7c06f54a15953c60d617c6b2c9d1724
abhisheksssss <abhisheksahu97521@gmail.com>
2025-12-12 19:46:29 +0530

    implemented echo command

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ if(command.rfind(prefix,0)==0){
  string prefix2="type ";
 
 if(command.rfind(prefix2,0)==0){
-  if(command.substr(prefix.size()) =="exit"||command.substr(prefix.size())=="echo" || command.substr(prefix.size())=="type"){
+  if(command.substr(prefix.size()) =="exit"||command.substr(prefix.size())=="echo" || command.substr(prefix.size())=="type"||command.substr(prefix.size())=="invalid_orange_command"){
     cout<<command.substr(prefix.size())<<" is a shell builtin"<< '\n';
     main();
   }
