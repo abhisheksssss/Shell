78762120bf4f553c8578717492b6cf8a5daa5527
abhisheksssss <abhisheksahu97521@gmail.com>
2025-12-12 18:39:01 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
+  
+  
+  
+  cout << unitbuf;
+  cerr << unitbuf;
+  
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
+
+  string command;
+  getline(cin,command);
+  cout<<command<<"Command not founded"<<endl;
+  
 }
