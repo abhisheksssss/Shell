33abfef11d631c05e4ba685c8609777af93f3840
abhisheksssss <abhisheksahu97521@gmail.com>
2025-12-12 18:54:34 +0530

    implemented main function

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,10 @@ int main() {
   cerr << unitbuf;
   
   // TODO: Uncomment the code below to pass the first stage
+ 
   cout << "$ ";
-
   string command;
   getline(cin,command);
   cout << command << ": command not found" << endl;
-
+  main();
 }
